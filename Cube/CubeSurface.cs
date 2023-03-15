d1aa9b0f3fcbf386be72341e36fdaa327038196e
F1st3K <kostinnikita988@gmail.com>
2023-03-15 19:34:51 +0300

    create architect Cube

---
+++ Cube/CubeSurface.cs
@@ -0,0 +1,33 @@
+﻿namespace Cube
+{
+    public sealed class CubeSurface<T>
+    {
+        private readonly T[][,] _surfaces;
+        
+        public CubeSurface(Dimension dimension)
+        {
+            Dimension = dimension;
+            _surfaces = new T[5][,];
+            _surfaces[0] = new T[Dimension.X, Dimension.Y];
+            _surfaces[1] = new T[Dimension.X, Dimension.Z];
+            _surfaces[2] = new T[Dimension.Y, Dimension.Z];
+            _surfaces[3] = new T[Dimension.Y, Dimension.X];
+            _surfaces[4] = new T[Dimension.Z, Dimension.X];
+            _surfaces[5] = new T[Dimension.Z, Dimension.Y];
+        }
+
+        public CubeSurface(int x, int y, int z)
+            : this(new Dimension(x, y, z))
+        {
+        }
+        
+        public Dimension Dimension { get; }
+
+        public T[,] this[int i]
+        {
+            get => _surfaces[i];
+            private set => _surfaces[i] = value;
+        }
+
+    }
+}
